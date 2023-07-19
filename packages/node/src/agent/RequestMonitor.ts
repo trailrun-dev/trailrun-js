b5ba572987b2f1ea757328c9a0416ef4d4da1848
Zev Stravitz <zev@rutterapi.com>
2023-07-18 20:10:19 -0400

    body types

--- packages/node/src/agent/RequestMonitor.ts
+++ packages/node/src/agent/RequestMonitor.ts
@@ -1,7 +1,7 @@
 import { BatchInterceptor } from '@mswjs/interceptors';
 import { ClientRequestInterceptor } from '@mswjs/interceptors/ClientRequest';
-import { FetchInterceptor } from '@mswjs/interceptors/Fetch';
 import { XMLHttpRequestInterceptor } from '@mswjs/interceptors/XMLHttpRequest';
+import { FetchInterceptor } from '@mswjs/interceptors/fetch';
 import { InteractiveRequest } from '@mswjs/interceptors/src/utils/toInteractiveRequest';
 import { DateTime } from 'luxon';
 import { LogPayload } from '../types';
@@ -11,114 +11,111 @@ import { Debugger } from './Debugger';
 import { LatencyMap } from './LatencyMap';
 
 export class RequestMonitor {
-	latencyMap: LatencyMap;
-	batchManager: BatchManager;
-	environment: string;
-	debug: Debugger;
+  latencyMap: LatencyMap;
+  batchManager: BatchManager;
+  environment: string;
+  debug: Debugger;
 
-	constructor(
-		latencyMap: LatencyMap,
-		batchManager: BatchManager,
-		debug: Debugger,
-		runtime: {
-			environment?: string;
-		},
-	) {
-		this.latencyMap = latencyMap;
-		this.batchManager = batchManager;
-		this.environment = runtime.environment ?? 'development';
-		this.debug = debug;
-	}
+  constructor(
+    latencyMap: LatencyMap,
+    batchManager: BatchManager,
+    debug: Debugger,
+    runtime: {
+      environment?: string;
+    },
+  ) {
+    this.latencyMap = latencyMap;
+    this.batchManager = batchManager;
+    this.environment = runtime.environment ?? 'development';
+    this.debug = debug;
+  }
 
-	async handleHttpRequest(args: {
-		request: InteractiveRequest;
-		requestId: string;
-	}): Promise<void> {
-		if (isTrailrunRequest(args.request)) {
-			return;
-		}
+  async handleHttpRequest(args: { request: InteractiveRequest; requestId: string }): Promise<void> {
+    if (isTrailrunRequest(args.request)) {
+      return;
+    }
 
-		this.latencyMap.setRequestResponse(args.requestId, DateTime.now().toISO());
-	}
+    this.latencyMap.setRequestResponse(args.requestId, DateTime.now().toISO());
+  }
 
-	async handleHttpResponse(args: {
-		response: Response;
-		isMockedResponse: boolean;
-		request: Request;
-		requestId: string;
-	}): Promise<void> {
-		const { response, request, requestId } = args;
-		if (isTrailrunRequest(request)) {
-			return;
-		}
+  async handleHttpResponse(args: {
+    response: Response;
+    isMockedResponse: boolean;
+    request: Request;
+    requestId: string;
+  }): Promise<void> {
+    const { response, request, requestId } = args;
+    if (isTrailrunRequest(request)) {
+      return;
+    }
 
-		let responseBody = '';
-		if (response.body && response.bodyUsed) {
-			responseBody = await streamToString(response.body.getReader());
-		}
+    let responseBody = '';
+    if (response.body && response.bodyUsed) {
+      responseBody = await streamToString(response.body.getReader());
+    }
 
-		const payloadResponse: LogPayload['response'] = {
-			message: response.statusText,
-			body: responseBody,
-			headers: response.headers,
-			statusCode: response.status,
-		};
+    const payloadResponse: LogPayload['response'] = {
+      message: response.statusText,
+      body: responseBody,
+      headers: response.headers,
+      statusCode: response.status,
+    };
 
-		let requestBody = '';
-		if (request.body && request.bodyUsed) {
-			requestBody = await streamToString(request.body.getReader());
-		}
+    let requestBody = '';
+    if (request.body && request.bodyUsed) {
+      requestBody = await streamToString(request.body.getReader());
+    }
 
-		const urlInterface = new URL(request.url);
+    const urlInterface = new URL(request.url);
 
-		const payloadRequest: LogPayload['request'] = {
-			method: request.method.toString() as LogPayload['request']['method'],
-			hostname: urlInterface.hostname,
-			headers: normalizeOutgoingHeaders(request.headers as globalThis.Headers),
-			body: requestBody,
-			search: urlInterface.search,
-			pathname: urlInterface.pathname,
-		};
+    const payloadRequest: LogPayload['request'] = {
+      method: request.method.toString() as LogPayload['request']['method'],
+      hostname: urlInterface.hostname,
+      headers: normalizeOutgoingHeaders(request.headers as globalThis.Headers),
+      body: requestBody,
+      search: urlInterface.search,
+      pathname: urlInterface.pathname,
+    };
 
-		const requestObject = this.latencyMap.getRequestResponse(requestId);
+    const requestObject = this.latencyMap.getRequestResponse(requestId);
 
-		if (!requestObject) {
-			return;
-		}
+    if (!requestObject) {
+      return;
+    }
 
-		const { callAt } = requestObject;
+    const { callAt } = requestObject;
 
-		const logPayload: LogPayload = {
-			request: payloadRequest,
-			response: payloadResponse,
-			callAt,
-			latencyInMilliseconds: DateTime.now().toMillis() - DateTime.fromISO(callAt).toMillis(),
-			environment: this.environment,
-		};
+    const logPayload: LogPayload = {
+      request: payloadRequest,
+      response: payloadResponse,
+      callAt,
+      latencyInMilliseconds: DateTime.now().toMillis() - DateTime.fromISO(callAt).toMillis(),
+      environment: this.environment,
+    };
 
-		this.latencyMap.clearRequestResponse(requestId);
+    this.latencyMap.clearRequestResponse(requestId);
 
-		if (this.batchManager.shouldAddToBatch(logPayload)) {
-			this.batchManager.addToBatch(logPayload);
-		} else {
-			this.debug.warn('Skipping log payload');
-		}
-	}
+    if (this.batchManager.shouldAddToBatch(logPayload)) {
+      this.batchManager.addToBatch(logPayload);
+    } else {
+      this.debug.warn('Skipping log payload');
+    }
+  }
 
-	instrumentHTTPTraffic() {
-		const interceptor = new BatchInterceptor({
-			name: 'trailrun-interceptor',
-			interceptors: [
-				new ClientRequestInterceptor(),
-				new XMLHttpRequestInterceptor(),
-				new FetchInterceptor(),
-			],
-		});
+  instrumentHTTPTraffic() {
+    const interceptor = new BatchInterceptor({
+      name: 'trailrun-interceptor',
+      interceptors: [
+        new ClientRequestInterceptor(),
+        new XMLHttpRequestInterceptor(),
+        new FetchInterceptor(),
+      ],
+    });
 
-		interceptor.on('request', this.handleHttpRequest.bind(this));
+    interceptor.on('request', this.handleHttpRequest.bind(this));
 
-		interceptor.on('response', this.handleHttpResponse.bind(this));
+    interceptor.on('response', this.handleHttpResponse.bind(this));
 
-		interceptor.apply();
-	}
+    interceptor.apply();
+  }
 }
--- packages/node/src/types.ts
+++ packages/node/src/types.ts
@@ -4,23 +4,23 @@ const headerSchema = z.record(any());
 const bodySchema = z.record(any());
 
 const logPayloadSchema = object({
-	request: object({
-		method: z.enum(['GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'OPTIONS', 'DELETE', 'TRACE']),
-		headers: headerSchema,
-		pathname: string(),
-		hostname: string(),
-		search: string().optional(),
-		body: any().optional(),
-	}),
-	response: object({
-		statusCode: number(),
-		headers: headerSchema,
-		message: string().optional(),
-		body: string().optional(),
-	}),
-	callAt: string(),
-	latencyInMilliseconds: number(),
-	environment: string(),
+  request: object({
+    method: z.enum(['GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'OPTIONS', 'DELETE', 'TRACE']),
+    headers: headerSchema,
+    pathname: string(),
+    hostname: string(),
+    search: string().optional(),
+    body: string().optional(),
+  }),
+  response: object({
+    statusCode: number(),
+    headers: headerSchema,
+    message: string().optional(),
+    body: string().optional(),
+  }),
+  callAt: string(),
+  latencyInMilliseconds: number(),
+  environment: string(),
 });
 
 type LogPayload = z.infer<typeof logPayloadSchema>;
